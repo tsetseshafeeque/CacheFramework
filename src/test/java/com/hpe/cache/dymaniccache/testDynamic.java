3fd11c4468dccd1f9c4ce72bc2c328d58824762b
nilayve7 <nilay.ve7@gmail.com>
2016-03-09 13:02:43 +0530

    Update testDynamic.java

--- src/test/java/com/hpe/cache/dymaniccache/testDynamic.java
+++ src/test/java/com/hpe/cache/dymaniccache/testDynamic.java
@@ -12,16 +12,12 @@ public class testDynamic{
 		DC.addCache(1, "piyush");
 		DC.addCache(2, "sam");
 		DC.addCache(3, "minu");
-		assertEquals(1,"piyush");
-		assertEquals(2,"sam");
-		assertEquals(3,"minu");
-		System.out.println("GetKey Testing");
-		System.out.println("added Key:" + DC.getCache(1));  
-		System.out.println("added Key:" + DC.getCache(2));
-		System.out.println("added Key:" + DC.getCache(3));
-		DC.update(2, "diya");
-		assertEquals(2,"diya");
-		System.out.println("updated key is:" +DC.getCache(2));
+		assertEquals("piyush",DC.getCache(1));
+		assertEquals("sam",DC.getCache(2));
+		assertEquals("minu",DC.getCache(3));
+	        DC.update(2, "diya");
+		assertEquals("diya",DC.getCache(2));
+	
 			
 	}
 
