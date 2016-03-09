43b54b9c3236c3b6cf8caf7c6108c8aba951c2a0
PiyushSadseliya <piyush.sadseliya@hotmail.com>
2016-03-08 22:24:40 -0800

    package

--- src/test/java/com/hpe/cache/dymaniccache/testDynamic.java
+++ src/test/java/com/hpe/cache/dymaniccache/testDynamic.java
@@ -1,33 +1,28 @@
 package com.hpe.cache.dymaniccache;
 
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 public class testDynamic{
 
 	@Test
-	public void testDynamic() 
+	public void test() 
 	{
 		DynamicCache DC = new DynamicCache(3);
 		System.out.println("AddKey Testing : Added");
 		DC.addCache(1, "piyush");
 		DC.addCache(2, "sam");
 		DC.addCache(3, "minu");
-		assertEquals(1,"piyush", 0);
-		assertEquals(2,"sam",0);
-		assertEquals(3,"minu",0);
+		assertEquals(1,"piyush");
+		assertEquals(2,"sam");
+		assertEquals(3,"minu");
 		System.out.println("GetKey Testing");
 		System.out.println("added Key:" + DC.getCache(1));  
 		System.out.println("added Key:" + DC.getCache(2));
 		System.out.println("added Key:" + DC.getCache(3));
 		DC.update(2, "diya");
-		assertEquals(2,"diya", 0);
+		assertEquals(2,"diya");
 		System.out.println("updated key is:" +DC.getCache(2));
 			
-	}
-
-	private void assertEquals(int i, String value, int j) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
