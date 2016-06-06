63f89b9bb31ee82f3be99bb353226bedfe01589f
PiyushSadseliya <piyush.sadseliya@hotmail.com>
2016-06-06 17:24:04 +0530

    CAC-3 DONE

--- src/main/java/com/hpe/cache/fifo/FifoCache.java
+++ src/main/java/com/hpe/cache/fifo/FifoCache.java
@@ -19,7 +19,7 @@ public class FifoCache extends LinkedHashMap<Integer, String> implements CacheIn
 
 	
 	public FifoCache(int capacity){
-		   super(capacity+1, 1.0f, true); 
+		   super(capacity+1, 1.0f, true); //TRIAL
 	       this.capacity = capacity;
 	   }
 	
